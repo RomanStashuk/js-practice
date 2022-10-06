8c26500446b0b973b56d1416097571eb5c175578
RomanStashuk <romanstashuk1@gmail.com>
2022-10-06 19:53:02 +0300

    add funcrions for checking brackets balance

---
+++ js/isBalanced/is-balanced.js
@@ -0,0 +1,15 @@
+const isBalanced = (expression) => {
+    const stack = [];
+    for (const symbol of expression) {
+        if (symbol === '(') {
+            stack.push(symbol);
+        } else if (symbol === ')') {
+            if (!stack.pop()) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(isBalanced('(()))))'));
